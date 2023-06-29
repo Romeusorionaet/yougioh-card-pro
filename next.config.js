94ddcbfa2a48e96c67f8e4fe5429ed4ac2592b6b
romeusorionaet <romeusoares14569@gmail.com>
2023-06-28 21:33:31 -0300

    change api

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'yougioh-api-cards.onrender.com',
-              port: '',
-              pathname: '/**',
-            },
-        ],
-    },
-  };
-  
-  module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yougioh-api-cards-production.up.railway.app',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
