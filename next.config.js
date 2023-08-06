44fb622437cbf1a2506d63e4aa132d68aa190c15
romeusorionaet <romeusoares14569@gmail.com>
2023-08-06 11:34:50 -0300

    change: config domains

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'https://yougioh-api-cards.onrender.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    domains: ['yougioh-api-cards.onrender.com'],
   },
 }
 
